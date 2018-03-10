35944c2ea65d52622feb4c6ccf047462c53d8f0b
Debora Csomos <csomidebi@gmail.com>
2018-03-10 22:38:45 +0100

    Corrected unchecked assignment.

--- overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/service/AIService.java
+++ overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/service/AIService.java
@@ -24,7 +24,7 @@ public class AIService {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
-        RequestEntity request = new RequestEntity(board, HttpMethod.POST, URI);
+        RequestEntity<ArrayList<String>> request = new RequestEntity<>(board, headers, HttpMethod.POST, URI);
         try {
             response = template.exchange(request, String.class).getBody();
         } catch (RestClientException e) {
