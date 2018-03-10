b885f91bbf67b5798b4e70608768076c3174185b
Debora Csomos <csomidebi@gmail.com>
2018-03-10 15:12:39 +0100

    Deleted unnecessary imports, simplified move method.

--- ticatactoe-ai-service/src/main/java/com/codecool/AI/controller/AIController.java
+++ ticatactoe-ai-service/src/main/java/com/codecool/AI/controller/AIController.java
@@ -1,25 +1,22 @@
 package com.codecool.AI.controller;
 
 import com.codecool.AI.model.Computer;
-import com.google.gson.Gson;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class AIController {
 
-    @Autowired
     private Computer computer;
 
+    private AIController() {
+        this.computer = new Computer("O", "Computer");
+    }
 
     @RequestMapping(value = "/move", method = RequestMethod.POST)
-    public ResponseEntity<List<String>> move(@RequestBody ArrayList<String> board) {
+    public @ResponseBody ArrayList<String> move(@RequestBody ArrayList<String> board) {
         board = computer.move(board);
-        return new ResponseEntity<>(board, HttpStatus.OK);
+        return board;
     }
 }
