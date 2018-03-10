705308ce6ff7c67497e0b4b2f2883a79865f2d8b
Debora Csomos <csomidebi@gmail.com>
2018-03-10 13:43:55 +0100

    Changed gameState to board.

--- overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/model/Player.java
+++ overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/model/Player.java
@@ -24,6 +24,6 @@ public class Player {
     }
 
     public void move(int position) {
-        game.changeGameState(position, character);
+        game.changeBoard(position, character);
     }
 }
--- overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/model/TicTacToeGame.java
+++ overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/model/TicTacToeGame.java
@@ -2,15 +2,15 @@ package com.codecool.enterprise.overcomplicated.model;
 
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @Component
 public class TicTacToeGame {
     private Map<String, Player> players = new HashMap<>();
-    private String[] gameState = new String[9];
+    private ArrayList<String> board = new ArrayList<>(9));
 
     public TicTacToeGame() {
+        Collections.fill(board, null);
         Player player = new Player("X", "Anonymous");
         player.setGame(this);
         players.put("player", player);
@@ -25,15 +25,17 @@ public class TicTacToeGame {
 
     public Computer getComputer() {
         return (Computer) players.get("computer");
+    public ArrayList<String> getBoard() {
+        return board;
     }
 
-    public String[] getGameState() {
-        return gameState;
+    public void setBoard(ArrayList<String> board) {
+        this.board = board;
     }
 
-    public void changeGameState(int position, String character) {
-        if (gameState[position] == null) {
-            gameState[position] = character;
+    public void changeBoard(int position, String character) {
+        if (board.get(position) == null) {
+            board.set(position, character);
         }
     }
 }
