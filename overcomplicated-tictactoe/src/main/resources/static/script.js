392acbc013141ad29e8be7e28e6aa94f54ba2e7c
Debora Csomos <csomidebi@gmail.com>
2018-04-06 12:07:50 +0200

    Added ajax for the game itself.

--- overcomplicated-tictactoe/src/main/resources/static/script.js
+++ overcomplicated-tictactoe/src/main/resources/static/script.js
@@ -2,11 +2,12 @@
 $(function () {
     funfactButton();
     comicEvent();
+    squareClickEvent();
 });
 
 function funfactButton() {
     $("#funFactButton").on("click", function () {
-        var funfactElement = $(".funFact");
+        let funfactElement = $(".funFact");
         $.ajax({
             type: "GET",
             url: "/new_fun_fact",
@@ -30,12 +31,53 @@ function comicEvent() {
             dataType: "json",
             contentType: "application/json",
             success: function (response) {
-                var img = $("#comic");
+                let img = $("#comic");
                 img.attr("src", response.uri);
                 img.attr("alt", response.alt);
                 img.attr("title", response.title);
             },
             error: function (response) {
+                console.log(response);
+            }
+        });
+    })
+}
+
+function squareClickEvent() {
+    $(".square").on("click", function () {
+        let target = event.target;
+        event.preventDefault();
+        let position = $(this).attr("id");
+        $.ajax({
+            type: "GET",
+            url: `/game-move?move=${position}`,
+            dataType: "json",
+            contentType: "application/json",
+            success: function (response) {
+                let board = JSON.parse(response["board"]);
+                console.log(board);
+                let squares = $(".square");
+
+                squares.each(function (index) {
+                    if (board[index] === "O") {
+                        $(this).addClass("fa fa-circle-o inactive");
+                    } else if (board[index] === "X") {
+                        $(this).addClass("fa fa-times inactive");
+                    }
+                });
+
+                if (response["winner"] != null || response["finished"]) {
+                    squares.each(function() {
+                        $(this).addClass("inactive");
+                    });
+                    $(target).addClass("fa fa-times");
+                    $("#finish-modal").modal("show");
+                    // return;
+
+                }
+            },
+            error: function (response) {
+                console.log(response);
             }
         });
     })
